a9e18f3d78fec60500baca7866d387029d151b05
Mikhail <grinfeld@gmail.com>
2018-11-04 23:36:56 +0200

    g

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,3 @@
-/*
-*
-* Copyright (c) 2015. Nexar Inc. - All Rights Reserved. Proprietary and confidential.
-*
-* Unauthorized copying of this file, via any medium is strictly prohibited.
-*
-*/
-
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
